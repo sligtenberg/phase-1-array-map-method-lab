b481d4b7f50c1a94b9f8efd025dc751733d84e53
stephen ligtenberg <bountyhunter24@gmail.com>
2022-10-03 10:52:00 -0400

    commit tree 9f8b3ff11fd3500b18accf19a38e8b830c022333

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// breakIntoWords will take a string and return an array with an element for each word in the oiginal string
+function breakIntoWords(inputString) {
+  const wordArray = [];
+  let nextWord = '';
+  for (let i in inputString) {
+    nextWord = nextWord.concat(inputString[i]);
+    if (inputString[i] === ' ' || Number(i) === inputString.length - 1) {
+      wordArray.push(nextWord);
+      nextWord = '';
+    }
+  }
+  return wordArray;
 }
+
+// takes elements and concatonates them all into one string
+function putBackTogether(inputArray) {
+  let line = '';
+  for (let i in inputArray) {
+    line = line.concat(inputArray[i]);
+  }
+  return line;
+}
+
+// takes a string and capitalizes the first letter
+function capFirstLetter (word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+// takes an array and capitalizes the first letter in each element
+function capFirstLetterInArray (arrayOfWords) {
+  return arrayOfWords.map(capFirstLetter);
+}
+
+// takes a 2D array and capitalizes each element in the inner array
+function capFirstLetter2AArray (arrayOfArraysOfWord) {
+  return arrayOfArraysOfWord.map(capFirstLetterInArray);
+}
+
+// const resultsPutTogether = capFirstLetter2AArray(tutorials.map(breakIntoWords)).map(putBackTogether);
+
+function titleCased () {
+  return capFirstLetter2AArray(tutorials.map(breakIntoWords)).map(putBackTogether);
+}
+
+console.log(titleCased());
